ba8e08042b43ef26c8effc0c84e16b3fdcfdcba3
Under <cabmanbellou@hotmail.com>
2020-10-17 20:07:32 -0500

    Updating now that conversion code is finished

---
+++ AddCountryColumn.py
@@ -0,0 +1,55 @@
+import reverse_geocoder
+import pandas as pd
+import numpy as np
+import json
+import country_converter
+
+#This Python script only needs to be run once, since it actually takes quite awhile to do.
+
+
+
+#Function to get the country from latitude and longitude coordinates
+def getplace(lat, lon):
+    coordinates = (float(lat), float(lon))
+    locationResults = reverse_geocoder.get(coordinates,mode=1)
+    some_names = [locationResults['cc']]
+    standard_names = country_converter.convert(names=some_names, to='name_short')
+    country = standard_names
+    return country
+
+some_names = ['us']
+test = country_converter.convert(names=some_names, to='name_short')
+
+earthquakefile = open("EarthquakeData.csv",'r')
+Lines = earthquakefile.readlines()
+writeLines = []
+firstIndex = 1
+totalLines = len(Lines)
+count = 0
+print("total lines to do:",totalLines)
+print("Be paitent, based on my calculations this takes 16-17 minutes on my Gaming PC, so it could be awhile.")
+for line in Lines:
+    if(firstIndex == 1):
+        firstIndex = 0
+        toWriteLine = "country," + line
+        writeLines.append(toWriteLine)
+    else:
+        if(count == 5853):
+            print("25% done")
+        elif(count == 11706):
+            print("50% done")
+        elif(count == 17559):
+            print("75% done")
+        lineArray = line.split(",")
+        lat = lineArray[2]
+        lon = lineArray[3]
+        country = getplace(lat, lon)
+        toWriteLine = country + "," + line
+        writeLines.append(toWriteLine)
+        count += 1
+earthquakefile.close()
+
+file2 = open('EarthquakeDataWithCountries.csv','w')
+file2.writelines(writeLines)
+file2.close()
+print("Finished creating converted file!")
---
+++ ConvertSuicideCountryFormat.py
@@ -0,0 +1,39 @@
+import country_converter
+
+file = open("SuicideData.csv",'r')
+Lines = file.readlines()
+writeLines = []
+count = 0
+for line in Lines:
+    if(count == 0):
+        print("Starting conversion")
+        print("This is going to take awhile (10+ minutes), so don't be suprised if it seems like nothing is happening.")
+    else:
+        if (count == 6955):
+            print("25% done")
+        elif (count == 13910):
+            print("50% done")
+        elif (count == 20865):
+            print("75% done")
+        lineArray = line.split(",")
+        some_names = [lineArray[0]]
+        standard_name = country_converter.convert(names=some_names, to='name_short')
+        lineArray[0] = standard_name
+        strToAdd = ""
+        firstIndex = 1
+        for entry in lineArray:
+            if(firstIndex == 1):
+                strToAdd = entry
+                firstIndex = 0
+            else:
+                strToAdd = strToAdd + "," + entry
+        writeLines.append(strToAdd)
+    count += 1
+
+file.close()
+
+file2 = open('SuicideDataContriesConverted.csv','w')
+file2.writelines(writeLines)
+file2.close()
+
+print("Finished converting all SuicideData country names")
